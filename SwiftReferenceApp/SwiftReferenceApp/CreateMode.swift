cb8097815dd211b81a1e620eec9da616df2033c8
GEORGE QUENTIN <ggeoo93@gmail.com>
2016-10-26 11:41:56 +0100

    added new files in Mode group

---
+++ SwiftReferenceApp/SwiftReferenceApp/CreateMode.swift
@@ -0,0 +1,9 @@
+//
+//  CreateMode.swift
+//  SwiftReferenceApp
+//
+//  Created by GEORGE QUENTIN on 26/10/2016.
+//  Copyright © 2016 StoryShare. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftReferenceApp/SwiftReferenceApp/ExploreMode.swift
@@ -0,0 +1,9 @@
+//
+//  ExploreMode.swift
+//  SwiftReferenceApp
+//
+//  Created by GEORGE QUENTIN on 26/10/2016.
+//  Copyright © 2016 StoryShare. All rights reserved.
+//
+
+import Foundation
